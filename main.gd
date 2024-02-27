0daee33ee182c735c5adc63fd2ef50e4a6cf1286
Omid Ahourai <oahourai@gmail.com>
2024-02-27 16:14:39 -0700

    random card gen

--- main.gd
+++ main.gd
@@ -1,14 +1,12 @@
 extends Node
 
-var CardDrawerNode: Node = load("res://card_drawer.tscn").instantiate()
-var Card: PackedScene = load("res://card.tscn")
+var card_scene: PackedScene = load("res://components/card/card.tscn")
+var card_drawer: Node
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	deal_cards(7)
-	#var card = Card.instantiate()
-	#add_child(card)
-	#card.position.x = Global.screen_center_x
-	#card.position.y = Global.screen_center_y
+	card_drawer = %CardDrawer
+	deal_cards(randi_range(3,10))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,18 +14,14 @@ func _process(delta):
 
 func deal_cards(count):
 	for i in count:
-		print(i)
 		draw_card(i)
 
 func draw_card(index):
-	var card = Card.instantiate()
-	add_child(card)
+	var card = card_scene.instantiate()
+	card_drawer.add_child(card)
 	var mod = floor(index/5)
-	var empty_card = CardDrawerNode.get_node(str("%EmptyCard",index+1))
-	#var empty_card = CardDrawerNode.get_node(str("%EmptyCard1"))
+	card_drawer.position.y = 300
+	var empty_card = card_drawer.get_node(str("%EmptyCard",index+1))
 	card.position.x = empty_card.position.x
 	card.position.y = empty_card.position.y
-	#card.position.x = index*(200+10) - (mod*index*(200+10))
-	#$EmptyCard1.position.y = 110 + (mod * 250)
-	#card.position.x = Global.screen_center_x
-	#card.position.y = Global.screen_center_y
+
